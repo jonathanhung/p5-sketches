19cbc49b2611605dbe49d622608a17af9119e3a2
Jonathan Hung MBP <hungjon@gmail.com>
2018-03-13 11:06:58 -0700

    updating to self portrait

---
+++ self-portrait/glasses.js
@@ -0,0 +1,21 @@
+var eye = {
+	x: 0,
+	y: 200,
+	diameter: 50
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  // background(0, 65, 2);
+}
+
+function draw() {
+  // put drawing code here
+  // ellipse, line, arc, point
+
+
+  // face
+  ellipse(200, 100, circle1.diameter);
+  
+}
