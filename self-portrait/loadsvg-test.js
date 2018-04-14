8f2186cbf19a83458647318fdd7cb1641f78d6c8
Jonathan Hung <hungjon@gmail.com>
2018-04-14 13:55:39 -0700

    svg test code

---
+++ self-portrait/loadsvg-test.js
@@ -0,0 +1,17 @@
+
+function preload() {
+	img = loadImage('images/test.svg');
+}
+
+function setup() {
+  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  // background(0, 65, 2);
+}
+
+function draw() {
+  // put drawing code here
+
+
+
+}
