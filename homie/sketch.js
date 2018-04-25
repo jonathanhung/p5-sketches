99adaecf530c5bcce47ec49d435bd5fe892984d6
Jonathan Hung <hungjon@gmail.com>
2018-04-24 18:51:36 -0700

    updates

--- homie/sketch.js
+++ homie/sketch.js
@@ -20,41 +20,74 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
+function showart(roommate) {
+  alert("Zlatan!");
+}
+
 
 function draw() {
   background(0);
   if (houseData) {
     for (var i = 0; i < houseData.length; i++) {
+      var numDays = 7;
       dateFrom = houseData[i].date;
+
+      // bind the date into an array [m, d, yyyy]
       d1 = dateFrom.split("/");
 
-      var from = new Date(d1[2], parseInt(d1[0])-1, d1[1]); 
-      var to = new Date();
-      to.setDate(from.getDate() + 7);
+      //
+      // new Date(year, month [, day [, hours [, minutes [, seconds [, milliseconds]]]]]);
+
+      var from = new Date(d1[2], parseInt(d1[0] - 1), d1[1]);
+      var to = new Date(from.getTime()+ ((1000*60*60*24) * numDays));
       var current = new Date();
 
+
+      // console.log("from date is:" + from);
+      // console.log("year: " + d1[2] + ", month: " + d1[0] + );
+
       if (current < to && current > from) {
-        console.log("from date is:" + from + ".. to date is:" + to);
+        var currentWeek = houseData[i];
+        if (currentWeek["AB"] == "x") {
+          alert("Ashley!");
+        } else if (currentWeek["ZK"] == "x") {
+          showart(zlatan);
+        } else if (currentWeek["JM"] == "x") {
+          alert("Justin!");
+        } else if (currentWeek["JH"] == "x") {
+          alert("Jonathan!");
+        } else if (currentWeek["JE"] == "x") {
+          alert("Jaylinn!");
+        }
+        // for (var n = 0; n < currentWeek.length; n++) {
+        //     console.log("hello!");
+        //       if (currentWeek[n]) {
+            //   console.log("it is the chore of the person in position: " + n);
+            // }
+        //   }
+
+        // check for Not undefined?
+        // check if property exists JSON. Key:value:
+
       }
 
-      // console.log(houseData[i].date);
-      // fill(255);
-      // ellipse(random(width), random(height), 20, 20);
 
-      // cmd + shift + P to show
     }
+
+    remove();
+
   }
 }
 
-// new Date(year, month [, day [, hours [, minutes [, seconds [, milliseconds]]]]]);
 
-// Sun Apr 08 2018 00:00:00 GMT-0700 (PDT)
-// Sun Mar 18 2018 00:00:00 GMT-0700 (PDT)
-// Sun Mar 25 2018 00:00:00 GMT-0700 (PDT)
 
-// sketch.js:37 from date is:Sun Mar 18 2018 00:00:00 GMT-0700 (PDT).. to date is:Wed Apr 25 2018 07:21:03 GMT-0700 (PDT)
-// sketch.js:37 from date is:Sun Mar 25 2018 00:00:00 GMT-0700 (PDT).. to date is:Wed May 02 2018 07:21:03 GMT-0700 (PDT)
-// sketch.js:37 from date is:Sun Apr 08 2018 00:00:00 GMT-0700 (PDT).. to date is:Sun Apr 15 2018 07:21:03 GMT-0700 (PDT)
-// sketch.js:37 from date is:Sun Mar 18 2018 00:00:00 GMT-0700 (PDT).. to date is:Wed Apr 25 2018 07:21:04 GMT-0700 (PDT)
-// sketch.js:37 from date is:Sun Mar 25 2018 00:00:00 GMT-0700 (PDT).. to date is:Wed May 02 2018 07:21:04 GMT-0700 (PDT)
-// sketch.js:37 from date is:Sun Apr 08 2018 00:00:00 GMT-0700 (PDT).. to date is:Sun Apr 15 2018 07:21:04 GMT-0700 (PDT
+// from date is:Sun Mar 18 2018 00:00:00 GMT-0700 (PDT).. to date is:Thu Apr 19 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Mar 25 2018 00:00:00 GMT-0700 (PDT).. to date is:Thu Apr 26 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Apr 01 2018 00:00:00 GMT-0700 (PDT).. to date is:Mon Apr 02 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Apr 08 2018 00:00:00 GMT-0700 (PDT).. to date is:Mon Apr 09 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Apr 15 2018 00:00:00 GMT-0700 (PDT).. to date is:Mon Apr 16 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Apr 22 2018 00:00:00 GMT-0700 (PDT).. to date is:Mon Apr 23 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun Apr 29 2018 00:00:00 GMT-0700 (PDT).. to date is:Mon Apr 30 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun May 06 2018 00:00:00 GMT-0700 (PDT).. to date is:Sat Apr 07 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun May 13 2018 00:00:00 GMT-0700 (PDT).. to date is:Sat Apr 14 2018 21:53:36 GMT-0700 (PDT)
+// sketch.js:36 from date is:Sun May 20 2018 00:00:00 GMT-0700 (PDT).. to date is:Sat Apr 21 2018 21:53:36 GMT-0700 (PDT)
---
+++ practice/sketch.js
@@ -0,0 +1,32 @@
+
+function setup() {
+  // put setup code here
+
+
+}
+
+function myTri(number) {
+  var output = "";
+  for (let i = 1; i <= number; i++) {
+    output = output + "#";
+    console.log(output);
+    };
+};
+
+myTri(7);
+console.log('test');
+
+//
+// var output;
+//
+// function triangle() {
+//   for (let i=1; i => 7; i++)
+//     {
+//
+//     }
+// }
+
+
+//
+// function draw() {
+// }
